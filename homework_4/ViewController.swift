4e72224e915d85e4e10496e1831de72e372f752b
Tsaorick <tsaorick@TsaorickdeMacBook-Pro.local>
2016-11-06 17:00:52 +0800

    Initial Commit

---
+++ homework_4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  homework_4
+//
+//  Created by Tsaorick on 2016/11/6.
+//  Copyright © 2016年 Tsaorick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
